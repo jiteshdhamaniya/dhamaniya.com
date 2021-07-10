444207abe8f32ab441b815246ec9b4e88e1a944c
Jitesh <jitesh@jdsoftvera.com>
2021-07-10 16:15:50 +0530

    added progressBar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import Footer from '../Components/Footer'
 import '../styles/globals.css'
+import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <NextNprogress
+      color="#29D"
+      startPosition={0.3}
+      stopDelayMs={200}
+      height={3}
+      showOnShallow={true}
+    />
       <div className="container max-w-[1024px] m-auto my-14 antialiased px-4 md:px-0"> 
       <Component {...pageProps} />
       <Footer />
      </div>
+     </>
   )
 }
 
