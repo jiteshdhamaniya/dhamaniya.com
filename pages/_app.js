d5eb7600b5c898fd692b49a96e8d34a239f3a7b9
Jitesh Dhamaniya <46275049+jiteshdhamaniya@users.noreply.github.com>
2022-03-09 12:22:55 +0530

    update next and remove progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,9 @@
 import Footer from '../Components/Footer'
 import '../styles/globals.css'
-import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-    <NextNprogress
-      color="#29D"
-      startPosition={0.3}
-      stopDelayMs={200}
-      height={3}
-      showOnShallow={true}
-    />
+    <>    
       <div className="container max-w-[1024px] m-auto my-14 antialiased px-2"> 
       <Component {...pageProps} />
       <Footer />
